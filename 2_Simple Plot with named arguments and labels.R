e078d71edc924265cfe947027048e57d17384333
Rahul <rahul@rahulpanda.in>
2018-02-24 12:38:36 +0530

    Simple Plot with named argument and labels

---
+++ 2_Simple Plot with named arguments and labels.R
@@ -0,0 +1,10 @@
+#Create a data frame
+df <- data.frame(
+  Name = c("a","b","c"),
+  Value = c(1,2,3)
+)
+#Display a data frame
+print(df)
+#Plot a data frame using named argument and labels
+plot(x=df$Name,y=df$Value,xlab="NAME",ylab="VALUE")
+
